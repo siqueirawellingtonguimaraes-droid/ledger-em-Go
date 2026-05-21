5a60ada4a57fe83f21ed395eadb8a2f3162e21ca
Wellington Siqueira <siqueirawellingtonguimaraes@gmail.com>
2026-05-21 20:28:31 -0300

    fix: update CORS configuration to allow all origins

--- cmd/main.go
+++ cmd/main.go
@@ -35,8 +35,7 @@ func main() {
 
 	r.Use(cors.New(cors.Config{
 		AllowOrigins: []string{
-			"http://localhost:5500",
-			"http://127.0.0.1:5500",
+			"*",
 		},
 		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:  []string{"Origin", "Content-Type", "Accept"},
